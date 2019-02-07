b9e789d1319f7de6d167b18bfc347cc01ccecbf5
manases <manases@DESENVOLVIMENTO-05>
2019-02-07 14:37:25 -0300

    motivo: melhorias WebService

--- webservice/src/br/com/webservice/resourses/ProdutoResourse.java
+++ webservice/src/br/com/webservice/resourses/ProdutoResourse.java
@@ -11,45 +11,45 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import br.com.webservice.model.domian.Produto;
 import br.com.webservice.service.ProdutoService;
+import javax.ws.rs.Consumes;
 
 @Path("/produtos")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class ProdutoResourse {
-
-	private ProdutoService service = new ProdutoService();
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public List<Produto> getProdutos() {
-		return service.getProdutos();
-	}
-
-	@GET
-	@Path("{produtoId}")
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public Produto getProduto(@PathParam("produtoId") long id) {
-		return service.getProduto(id);
-	}
-
-	@POST
-	public Response save(Produto produto) {
-		produto = service.saveProduto(produto);
-		return Response.status(Status.CREATED).entity(produto).build();
-	}
-
-	@PUT
-	@Path("{produtoId}")
-	public void update(@PathParam("produtoId") long id, Produto produto) {
-		produto.setId(id);
-		service.updateProduto(produto);
-	}
-
-	@DELETE
-	@Path("{produtoId}")
-	public void delete(@PathParam("produtoId") long id) {
-		service.deleteProduto(id);
-	}
+  
+       private ProdutoService service = new ProdutoService();
+        
+       @GET
+       public List<Produto> getProdutos() {
+             return service.getProdutos();
+       }
+        
+       @GET
+       @Path("{produtoId}")
+       public Produto getProduto(@PathParam("produtoId") long id) {
+             return service.getProduto(id);
+       }
+        
+       @POST
+       public Response save(Produto produto) {
+              produto = service.saveProduto(produto);
+              return Response.status(Response.Status.CREATED).entity(produto).build();
+       }
+        
+       @PUT
+       @Path("{produtoId}")
+       public void update(@PathParam("produtoId") long id, Produto produto) {
+             produto.setId(id);
+             service.updateProduto(produto);
+       }
+        
+       @DELETE
+       @Path("{produtoId}")
+       public void delete(@PathParam("produtoId") long id) {
+             service.deleteProduto(id);
+       }
 }
